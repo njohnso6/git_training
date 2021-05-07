b36a663a744be8688c9756b2df8389ae812ff86c
Nicholas Johnson <njohnso6@uccs.edu>
2021-05-07 10:43:00 -0400

    More Ryan Edits.

--- merge_module/my_pride.R
+++ merge_module/my_pride.R
@@ -3,9 +3,10 @@
 contr_a_norm_WT <- make_contrast('treatment', 'norm', 'WT')
 
 # Here we do some neat stuff with the contrast and get a
-# a list of jeans
+# a list of jeans.
 df_sel <- contra_a_norm_WT[rows_I_want,]
 
 # Below we will make a ggplot of the data
-ggplot(df_sel, aes(displ, hwy, colour = class)) + 
-  geom_point()
+ggplot(df_sel, aes(mean, logfoldchange, colour = group)) + 
+  geom_point() +
+  geom_line()
