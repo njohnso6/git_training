96ea256a9146afe3243414a4778ed87c57733ca5
Nicholas Johnson <njohnso6@uccs.edu>
2021-05-07 10:21:22 -0400

    Initial commit of my_pride.R.

---
+++ merge_module/my_pride.R
@@ -0,0 +1,9 @@
+# This code will create a ggplot representing the contrast
+# between two conditions in the rnaseq.Rmd
+contr_a_norm_WT <- make_contrast('treatment', 'norm', 'WT')
+# Here we do some neat stuff with the contrast and get a
+# a list of jeans
+df_sel <- contra_a_norm_WT[rows_I_want,]
+# Below we will make a ggplot of the data
+ggplot(df_sel, aes(displ, hwy, colour = class)) + 
+  geom_point()
